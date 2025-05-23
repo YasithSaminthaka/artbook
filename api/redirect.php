3ae0fa40bb423543347fee6574f8b250a45217eb
YasithSaminthaka <yasithsaminthaka@gmail.com>
2025-05-23 13:53:44 +0530

    cc

--- api/redirect.php
+++ api/redirect.php
@@ -1,6 +1,8 @@
 <?php 
 
 require_once '../URI.php';
+?>
+
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         submitData();
@@ -8,6 +10,7 @@ require_once '../URI.php';
         async function submitData() {
             const queryString = window.location.search;
             const targetUrl = `http://localhost/artbook/api/google_callback.php${queryString}`;
+            const targetUrl = `${SITE_URL}/api/google_callback.php${queryString}`;
 
             try {
                 const response = await fetch(targetUrl, {
@@ -30,12 +33,15 @@ require_once '../URI.php';
                         if (data.email) localStorage.setItem('artbook_user_email', data.email);
                     }
                     window.location.href = 'http://localhost/artbook/index.php';
+                    window.location.href = "<?= SITE_URL ?>/index.php";
 
                 } else {
                     window.location.href = 'http://localhost/artbook/index.php';
+                    window.location.href = "<?= SITE_URL ?>/index.php";
                 }
             } catch (error) {
                 window.location.href = 'http://localhost/artbook/index.php';
+                window.location.href = "<?= SITE_URL ?>/index.php";
             }
         }
 
